3f9704bacd8e511572fb66888ea21ae61a2ebecb
VitoriaCanon <vitoriacanon1212@gmail.com>
2025-08-22 15:02:42 -0300

    Exercicio 1

---
+++ src/lista01/exercicio01/Main.java
@@ -0,0 +1,24 @@
+package lista01.exercicio01;
+
+public class Main {
+    package lista01.exercicio01;
+
+    public class Main {
+        public static void main(String[] args) {
+//       constante no Java se identifica por FINAL
+            final float PI = 3.14f;
+
+            System.out.println(soma(2, 2));
+            System.out.println(subtracao(2, 2));
+        }
+
+        public static int soma(int a, int b ) {
+            return a + b;
+        }
+
+        public  static int subtracao (int a, int b) {
+            return a - b;
+        }
+    }
+
+}
