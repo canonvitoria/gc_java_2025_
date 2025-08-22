c090e7e57aad29a29e6010b1fe8f3ed344d25631
VitoriaCanon <vitoriacanon1212@gmail.com>
2025-08-22 18:42:08 -0300

    Exercicio 2

---
+++ src/lista01/exercicio02/Main.java
@@ -0,0 +1,37 @@
+package lista01.exercicio02;
+import java.util.Scanner;
+
+// Crie uma variável com seu respectivo tipo para todos os itens:
+//-Um nome;
+//-Uma idade;
+//-Se uma pessoa possuí cadastro.
+//-Um cpf;
+//-Um telefone;
+//-Uma altura em metros;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String nome;
+        int idade;
+        String possuiCadastro;
+        int cpf;
+        int telefone;
+        int altura;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nome:");
+        scanner.nextLine();
+        System.out.println("Idade: ");
+        scanner.nextInt();
+        System.out.println("Possui cadastro? (N/S): ");
+        scanner.next();
+        System.out.println("CPF: ");
+        scanner.next();
+        System.out.println("Telefone: ");
+        scanner.next();
+        System.out.println("Altura (em metros): ");
+        scanner.next();
+    }
+
+}
