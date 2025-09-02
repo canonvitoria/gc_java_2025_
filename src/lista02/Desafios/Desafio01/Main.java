77b34a1352352defd5da28257590f4366ff1079e
VitoriaCanon <vitoriacanon1212@gmail.com>
2025-09-02 01:36:09 -0300

    desafio 02 lista 02

--- src/lista02/Desafios/Desafio01/Main.java
+++ src/lista02/Desafios/Desafio01/Main.java
@@ -1,24 +1,64 @@
-package lista02.Desafios.Desafio01;
-
-import java.util.Scanner;
+package lista02.Desafios.Desafio01; // Estudo!
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int [][] matriz = new int [3][3];
 
-//        int linha = 0;
-//        int coluna = 0;
+        // 1. Declaração e Inicialização de uma matriz 3x4
+        // A sintaxe tipo[][] indica uma matriz, e new int[3][4] cria 3 linhas e 4 colunas.
+        int[][] matriz = new int[3][4];
 
-        for (int linha = 0; linha < matriz.length; linha++)  {
-            System.out.println(linha);
-            for (int coluna = 0; coluna < matriz.length; coluna++) {
-                System.out.println(linha);
+        // 2. Acessando e preenchendo elementos
+        // Para acessar um elemento, use o índice da linha e depois o da coluna.
+        // Lembre-se que os índices começam em 0.
+        matriz[0][0] = 10; // Linha 1, Coluna 1
+        matriz[1][2] = 25; // Linha 2, Coluna 3
+        matriz[2][3] = 40; // Linha 3, Coluna 4
+
+        System.out.println("--- Elementos específicos da matriz ---");
+        System.out.println("Elemento na posição [0][0]: " + matriz[0][0]);
+        System.out.println("Elemento na posição [1][2]: " + matriz[1][2]);
+        System.out.println("Elemento na posição [2][3]: " + matriz[2][3]);
+        System.out.println();
+
+        // 3. Preenchendo a matriz usando laços aninhados (o método mais comum)
+        int[][] matrizDinamica = new int[3][3];
+        int contador = 1;
+
+        // O laço externo percorre as linhas
+        for (int i = 0; i < matrizDinamica.length; i++) {
+            // O laço interno percorre as colunas
+            for (int j = 0; j < matrizDinamica[i].length; j++) {
+                matrizDinamica[i][j] = contador;
+                contador++;
             }
-       }
+        }
 
-//        for(int i = 0; i < matriz.length; i++) {
-//            System.out.println(matriz[i][i]);
-//        }
+        System.out.println("--- Matriz preenchida dinamicamente (3x3) ---");
+        // Chama a função para imprimir a matriz de forma organizada
+        imprimirMatriz(matrizDinamica);
+        System.out.println();
+
+        // 4. Inicialização direta (para valores conhecidos)
+        // Você pode criar e preencher a matriz na mesma linha.
+        int[][] matrizDireta = {
+                {5, 10, 15},
+                {20, 25, 30},
+                {35, 40, 45}
+        };
+
+        System.out.println("--- Matriz com inicialização direta ---");
+        imprimirMatriz(matrizDireta);
+    }
+
+    // Método auxiliar para imprimir qualquer matriz de inteiros de forma organizada
+    public static void imprimirMatriz(int[][] mat) {
+        // Percorre cada linha da matriz
+        for (int i = 0; i < mat.length; i++) {
+            // Percorre cada elemento dentro da linha atual
+            for (int j = 0; j < mat[i].length; j++) {
+                System.out.print(mat[i][j] + "\t"); // \t adiciona um espaçamento de tabulação
+            }
+            System.out.println(); // Quebra de linha para começar a próxima linha da matriz
+        }
     }
 }
---
+++ src/lista02/Desafios/Desafio02/Main.java
@@ -0,0 +1,81 @@
+package lista02.Desafios.Desafio02;
+
+// Em dupla - Implemente uma calculadora, onde o usuário deve entrar com 2 números e escolher a operação que deseja realizar (soma, subtração, divisão, multiplicação,
+// exponenciação e raiz quadrada). Durante a execução, o programa deve perguntar ao usuário se ele deseja continuar, se não quiser, deve digitar ‘n’ para o programa encerrar.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        calculadora();
+    }
+
+    public static void calculadora() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        int primeiroNum = sc.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        int segundoNum = sc.nextInt();
+
+        System.out.println("Qual operação você deseja? " +
+                "1 - Soma " +
+                "2 - Subtração " +
+                "3 - Divisão " +
+                "4 - Multiplicação " +
+                "5 - Exponenciação " +
+                "6 - Raiz Quadrada ");
+        int operacao = sc.nextInt();
+
+        switch (operacao) {
+            case 1:
+                int soma = primeiroNum + segundoNum;
+                System.out.println("A soma entre 2 números é: " + soma);
+                break;
+            case 2:
+                int subtracao = primeiroNum - segundoNum;
+                System.out.println("A subtração entre 2 números é: " + subtracao);
+                break;
+            case 3:
+                int divisao = primeiroNum / segundoNum;
+                System.out.println("A divisão entre 2 números é: " + divisao);
+                break;
+            case 4:
+                int multiplicacao = primeiroNum * segundoNum;
+                System.out.println("A multiplicação entre 2 números é: " + multiplicacao);
+                break;
+            case 5:
+                double exponenciacao = 1;
+                for (int i = primeiroNum; i <= segundoNum + 1; i++) {
+                    exponenciacao = exponenciacao * primeiroNum;
+                    System.out.println(exponenciacao + " x ");
+                }
+                // 2 5
+                // 2 2 2 2 2
+                break;
+            case 6:
+                double inicio = 0;
+                double fim = primeiroNum;
+                double precisao = 0.00001; // Pra ter um resultado com 5 casas decimais
+
+                while ((fim - inicio) > precisao) {
+                    double meio = (inicio + fim) / 2;
+                    double quadrado = meio * meio;
+
+                    if (quadrado == precisao) {
+                        System.out.println("A raiz é: " + meio);
+                        return;
+                    } else if (quadrado < primeiroNum) {
+                        inicio = meio;
+                    } else {
+                        fim = meio;
+                    }
+                }
+                System.out.println("A raiz aproximada é: " + inicio);
+                break;
+            default:
+                System.out.println(">> Insira um valor válido!!");
+        }
+    }
+}
