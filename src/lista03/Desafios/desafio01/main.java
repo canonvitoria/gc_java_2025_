53cac605abc11839dd09747ea7a7de6f3113c32b
canonvitoria <vitoriacanon1212@gmail.com>
2025-09-16 17:12:14 -0300

    lista 03

---
+++ src/lista03/Desafios/desafio01/main.java
@@ -0,0 +1,23 @@
+package lista03.Desafios.desafio01;
+
+// Crie uma classe ContaBancaria com os atributos:
+    //• nome (nome do titular da conta)
+    //• cpf (número de CPF do titular)
+    //• identificadorConta (número de identificação da conta)
+    //• banco (nome do banco)
+    //• endereco (endereço do titular)
+    //• saldo (saldo disponível na conta)
+    //• horarioAtual (um atributo para armazenar o horário atual)
+
+// Implemente os seguintes métodos na classe:
+    //• saque(): Realiza um saque da conta, desde que o saldo seja suficiente. Caso contrário, exibe uma mensagem de erro.
+    //• deposito(): Realiza um depósito na conta, atualizando o saldo.
+    //• pix(): Realiza uma transferência PIX. Caso contrário, exibe uma mensagem de erro.
+    //• transferencia(): Realiza uma transferência para outra conta bancária, verificando se o horário atual está dentro do intervalo permitido (8hrs às 19hrs) e se o saldo é suficiente. Caso contrário, exibe uma mensagem de erro.
+    //• verificarSaldo(): Exibe o saldo atual da conta.
+    //• verificarHorario(): Exibe o horário atual.
+    //• verificarInformacoes(): Exibe todas as informações da conta bancária, como nome, CPF, saldo, etc.
+
+
+public class main {
+}
---
+++ src/lista03/Exercicios/exercicio01/main.java
@@ -0,0 +1,7 @@
+package lista03.Exercicios.exercicio01;
+
+//  Escolha um objeto da sala, crie a classe com as características e
+//  comportamentos que você julgue necessários.
+
+public class main {
+}
