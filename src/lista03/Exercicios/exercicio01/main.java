4da44a0e6a35f28db0e6db5a286e42d9335a7a50
Vitoria Canon <vitoriacanon1212@gmail.com>
2025-09-17 22:21:50 -0300

    lista 03 - exercicio 01

--- src/lista03/Exercicios/exercicio01/main.java
+++
@@ -1,7 +0,0 @@
-package lista03.Exercicios.exercicio01;
-
-//  Escolha um objeto da sala, crie a classe com as características e
-//  comportamentos que você julgue necessários.
-
-public class main {
-}
